f7ea46c2399f40b5813e95b2eed221b5bbea7746
crysler <crysler@ubiot.io>
2017-02-15 15:59:05 -0400

    restructurando el codigo para llamar funciones

--- obtener_lista_amigos_facebook_rentadvisor/obtener_lista_amigos_facebook_rentadvisor.php
+++ obtener_lista_amigos_facebook_rentadvisor/obtener_lista_amigos_facebook_rentadvisor.php
@@ -13,23 +13,33 @@ $request = new Facebook\FacebookRequest(
 			['fields' => 'birthday,location,name,id,hometown,email'
 		  ]);
 
-		  
-// Send the request to Graph
-try {
-  $response = $fb->getClient()->sendRequest($request);
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-  // When Graph returns an error
-  echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-  // When validation fails or other local issues
-  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
-} 
+// con esta funcion permito hacer varios llamados sin repetir codigo
+function retornarGrafo($request,$fb){
+	$response ="";
+	// Send the request to Graph
+	try {
+	  $response = $fb->getClient()->sendRequest($request);
+	  
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+	  // When Graph returns an error
+	  echo 'Graph returned an error: ' . $e->getMessage();
+	  exit;
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+	  // When validation fails or other local issues
+	  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+	  exit;
+	} 
+return $response;
+
+}
  
+	
+//haciendo el request, y guardandolo en otra variable para usarlo luego
+$response=retornarGrafo($request,$fb);
  
-// you have the latest dev version of the official SDK
+//llamando al objeto solicitado
 $graphEdge = $response->getGraphEdge();
+
  for ($i=0;$i< sizeOf($graphEdge);$i++){
     print_r($graphEdge[$i]);
 	echo "<br>";
