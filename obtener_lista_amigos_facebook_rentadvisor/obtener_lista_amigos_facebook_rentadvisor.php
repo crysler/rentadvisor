970c1b4177f9dacdc3f515507f02721b729159e7
crysler <crysler@ubiot.io>
2017-02-15 15:50:08 -0400

    recorriendo el objeto internamente

--- obtener_lista_amigos_facebook_rentadvisor/obtener_lista_amigos_facebook_rentadvisor.php
+++ obtener_lista_amigos_facebook_rentadvisor/obtener_lista_amigos_facebook_rentadvisor.php
@@ -1,14 +1,19 @@
 <?php
 session_start();
 //verificar esta variable de sesion 
-$_SESSION['fb_access_token']="258909491147805|WlrtIwRuahmP7sJboxbJKsdKLEs";
-
-require_once __DIR__ .'/php-graph-sdk-5.4/src/Facebook/autoload.php';
- 
-use Facebook\FacebookRequest;
 
 
-/* 
+include_once "sensivility_data.php"; 
+
+$request = new Facebook\FacebookRequest(
+          $fbApp,
+		  $_SESSION['fb_access_token'],
+		  'GET',
+		  "/".$id_user."/friends",
+			['fields' => 'birthday,location,name,id,hometown,email'
+		  ]);
+
+		  
 // Send the request to Graph
 try {
   $response = $fb->getClient()->sendRequest($request);
@@ -20,15 +25,13 @@ try {
   // When validation fails or other local issues
   echo 'Facebook SDK returned an error: ' . $e->getMessage();
   exit;
-}*/
+} 
  
-$graphNode = $response->getGraphNode();
-  
-/*
-echo 'User name: ' . $graphObject['name'];
-echo 'birthday: ' . $graphObject['birthday'];
-*/
-print_r($graphNode);
-
+ 
+// you have the latest dev version of the official SDK
+$graphEdge = $response->getGraphEdge();
+ for ($i=0;$i< sizeOf($graphEdge);$i++){
+    print_r($graphEdge[0]);
+ }
 
 ?>
